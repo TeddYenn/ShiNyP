0382a9a874cb3782ccc58de337d9f4e773011e93
TeddYenn <158191417+TeddYenn@users.noreply.github.com>
2024-08-19 19:08:23 +0800

    20240819-5

--- R/run_ShiNyP.R
+++ R/run_ShiNyP.R
@@ -1,9 +1,8 @@
 # R/run_ShiNyP.R
 run_ShiNyP <- function() {
-  appDir <- system.file("app", package = "MyShinyApp")
+  appDir <- system.file("app", package = "ShiNyP")
   if (appDir == "") {
-    stop("Could not find app directory. Try re-installing `MyShinyApp`.", call. = FALSE)
+    stop("Could not find app directory. Try re-installing `ShiNyP`.", call. = FALSE)
   }
-  
   shiny::runApp(appDir, display.mode = "normal")
 }
