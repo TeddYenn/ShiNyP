c9f02cf120541baa4f95cf8e930bcd180f45e5bd
TeddYenn <158191417+TeddYenn@users.noreply.github.com>
2024-10-12 15:08:12 +0800

    v0.1.0_20241012

---
+++ R/constants.R
@@ -0,0 +1,34 @@
+Tree_layout_choice = c("Rectangular" = "rectangular",
+                       "Round rectangular" = "roundrect",
+                       "Dendrogram" = "dendrogram",
+                       "Ellipse" = "ellipse",
+                       "Circular" = "circular",
+                       "Inward circular" = "inward_circular",
+                       "Radial" = "radial",
+                       "Equal angle" = "equal_angle")
+
+Legend_choice = c("Top" = "top",
+                  "Bottom" = "bottom",
+                  "Right" = "right",
+                  "Left" = "left",
+                  "None" = "none")
+
+GT_method_choice = c("Cavalli-Sforza's chord distance" = "Dch",
+                     "Nei's genetic distance" = "Da",
+                     "Nei's standard genetic distance" = "Ds",
+                     "Reynolds's genetic distance" = "Fst",
+                     "Nei's minimum distance" = "Dm",
+                     "Rogers's distance" = "Dr",
+                     "Prevosti's distance" = "Cp",
+                     "Sanghvi's distance" = "X2")
+
+pval_adj_method_choice = c("Bonferroni" = "bonferroni",
+                           "Benjamini & Hochberg (FDR)" = "BH")
+
+AI_model_choice = c("GPT-4o" = "gpt-4o",
+                    "GPT-4o mini" = "gpt-4o-mini",
+                    "GPT-4 Turbo" = "gpt-4-turbo-2024-04-09",
+                    "GPT-4" = "gpt-4",
+                    "GPT-3.5 Turbo" = "gpt-3.5-turbo")
+
+custom_palette = c("#4482a8", "#91bb67", "#e3b800", "#E85C0D", "#e9788e", "#c493ff")
